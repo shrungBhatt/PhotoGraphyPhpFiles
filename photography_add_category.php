cb900660897ac40077162463915537716f2aae18
Shrung Bhatt <huntjigsaw97@gmail.com>
2018-03-23 00:24:21 +0530

    Added uploading image file

--- photography_add_category.php
+++ photography_add_category.php
@@ -2,9 +2,15 @@
 require "conn.php";
 
 $photo_category = $_POST["photo_category"];
-$photo_url = $_POST["photo_url"];
+$image = $_POST["image"];
 $create_date = date('Y-m-d H:i:s');
 
+
+
+
+$path = "photography/categories/$photo_category.png";
+
+$actualpath = "http://ersnexus.esy.es/$path";
 
 $mysql_qry = "INSERT INTO photography_photo_categories
 (photo_category,
@@ -12,10 +18,13 @@ photo_url,
 create_date) 
 VALUES 
 ('$photo_category',
-'$photo_url',
+'$actualpath',
 '$create_date')";
 
+
+
 if($conn->query($mysql_qry) === TRUE){
+    file_put_contents($path,base64_decode($image));
     $result = "Insert Successful";
 	echo json_encode(array("data" => array("result" => $result)));
 }else{
