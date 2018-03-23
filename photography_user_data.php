b6a38e4dad721411014d07d296d02994c2f96afd
Shrung Bhatt <huntjigsaw97@gmail.com>
2018-03-23 16:46:17 +0530

    Updated the login file

--- photography_user_data.php
+++ photography_user_data.php
@@ -7,10 +7,11 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $dob = $_POST["dob"];
 $phoneNo = $_POST["phone_no"];
-$gender = $_POST["gender"]
+$gender = $_POST["gender"];
+$userType = $_POST["user_type"];
 $date = date('Y-m-d H:i:s');
 
-$mysql_qry = "insert into photography_user_data(email_id, user_name, password, create_date) values ('$mac_id','$username','$password','$date')";
+$mysql_qry = "insert into photography_user_data(email_id, user_name, password, create_date,user_type) values ('$mac_id','$username','$password','$date','$userType')";
 
 if($conn->query($mysql_qry) === TRUE){
 	echo "Insert SuccessFul";
--- photography_user_login.php
+++ photography_user_login.php
@@ -3,8 +3,9 @@ require "conn.php";
 
 $user_name = $_POST["username"]; //funcion used to add slash before any wild card characters.
 $user_pass = $_POST["password"];
+$user_type = $_POST["user_type"];
 
-$mysql_qry = "SELECT * FROM photography_user_data WHERE user_name='$user_name' AND password='$user_pass'";
+$mysql_qry = "SELECT * FROM photography_user_data WHERE user_name='$user_name' AND password='$user_pass' AND user_type='$user_type'";
 
 $result = mysqli_query($conn, $mysql_qry);
 
